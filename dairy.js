46d037862b83b3a1652b7c8adf1b873d77295c8f
harshavalluri2007 <145581425+harshavalluri2007@users.noreply.github.com>
2024-05-27 11:25:22 +0530

    Delete dairy.js

--- dairy.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-            const bg = document.getElementById('header');
-            window.addEventListener('scroll',function(){
-                bg.style.backgroundSize = 160 - + window.pageYOffset/25+'%'
-            });
-        </script>
